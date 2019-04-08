7d37b3d7eb9d20946049765cb43d9b2cb8d20ea5
unknown <cristianfelipe.guerrero@gmail.com>
2019-04-08 17:51:02 -0500

    Revert "Completed the module"
    This reverts commit 8235e8074859cb5b5dbf6d83d3973ee726d7066c.

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion.js';
-import QuizEnd from './QuizEnd.js';
 
 let quizData = require('./quiz_data.json');
 
@@ -10,19 +9,11 @@ class Quiz extends Component {
     this.state = { quiz_position: 1 };
   }
   render() {
-    const isQuizEnd =
-      this.state.quiz_position - 1 === quizData.quiz_questions.length;
     return (
       <div>
-        {isQuizEnd ? (
-          <QuizEnd />
-        ) : (
-          <QuizQuestion
-            quiz_question={
-              quizData.quiz_questions[this.state.quiz_position - 1]
-            }
-          />
-        )}
+        <QuizQuestion
+          quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+        />
       </div>
     );
   }
